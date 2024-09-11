dd4040efa437bbbe87d2045243165a9d69e0067e
Anatolek27 <anatole.kunegel@gmail.com>
2024-09-11 09:50:27 +0200

    added maps and recovered last commit that created rgb checks fail

--- srcs/main.c
+++ srcs/main.c
@@ -8,9 +8,5 @@ int main(int argc, char **argv)
 		return (ft_exit(&data, "Error : Expected exactly 1 argument"));
 	data.paths.pfile = argv[1];
 	parsing(&data);
-	for (int i = 0; data.map.map[i]; i++)
-		printf("%s\n", data.map.map[i]);
-	printf("Textures: %s\n%s\n%s\n%s\n", data.paths.pn, data.paths.ps, data.paths.pw, data.paths.pe);
-	printf("C_HEX: %#X\nF_HEX: %#X", data.t.rgbc_hex, data.t.rgbf_hex);
 	return (ft_exit(&data, "Game closed"));
 }
